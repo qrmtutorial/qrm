986d5e70978d61b19f85797cb8bc5bd4777143da
Alexander John McNeil <alexanderjmcneil@gmail.com>
2016-01-06 13:42:18 +0000

    commit copula scripts

--- R/07_Copulas_and_Dependence/07_Copula_estimation.R
+++ R/07_Copulas_and_Dependence/07_Copula_estimation.R
@@ -1,28 +1,41 @@
+# by Alexander McNeil
 library(QRM) # for fitting functions (to change)
 library(copula) # for simulation
-
+library(qrmdata) # for data
+library(xts)
 #### COPULA FITTING
 
 # Fitting copulas to data
 
-load("INDEXES-2000-2012.RData")
-plot(INDEXES0012)
-Xdata <- returns(INDEXES0012[,c(1,3)])
-plot(Xdata)
-plot(series(Xdata))
-dim(Xdata)
+data("SP500")
+data("FTSE")
+# compute returns
+SP500.X <- diff(log(SP500))[-1]
+FTSE.X <- diff(log(FTSE))[-1]
 
+INDEXES.X <- merge(SP500.X,FTSE.X,all=FALSE)
+INDEXES.X <- INDEXES.X['2003-01-01/2012-12-31',]
+plot.zoo(INDEXES.X)
+pairs(as.zoo(INDEXES.X))
+
+# observe that there are zero values caused by market closures
+exclude.rows <- (INDEXES.X[,1]==0) | (INDEXES.X[,2]==0)
+sum(exclude.rows)
+# remove these
+INDEXES.X.d <- INDEXES.X[!exclude.rows,]
+# Aggregating by week
+INDEXES.X.w <- apply.weekly(INDEXES.X.d,FUN=colSums)
+plot.zoo(INDEXES.X.w,type="h")
+dim(INDEXES.X.w)
+
+
+
+Xdata <- INDEXES.X.w
 # Construct pseudo copula data
 
 Udata <- apply(Xdata,2,edf,adjust=1)
-plot(series(Udata))
-# observe that there are tied values caused by holidays
-exclude.rows <- (Xdata[,1]==0) | (Xdata[,2]==0)
-sum(exclude.rows)
-Xdata <- Xdata[!exclude.rows,]
-Udata <- apply(Xdata,2,edf,adjust=1)
-plot(series(Udata))
-
+plot.zoo(Udata)
+plot(Udata[,1],Udata[,2])
 
 # Compare various bivariate models
 
@@ -32,12 +45,10 @@ mod.gumbel <- fit.AC(Udata,"gumbel")
 mod.gumbel
 mod.clayton <- fit.AC(Udata,"clayton")
 mod.clayton
-mod.t <- fit.tcopula(Udata)
+mod.t <- fit.tcopula(Udata, startdf =3)
 mod.t
 
 c(mod.gauss$ll.max, mod.t$ll.max, mod.gumbel$ll.max, mod.clayton$ll.max)
-
-
 
 # Multivariate Fitting with Gauss and t: Simulation study
 # We check whether we can recover true parameters
