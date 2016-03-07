1e916d5634bd3fbbb2b0c873b2a1629ecf2519e7
Alexander John McNeil <alexanderjmcneil@gmail.com>
2016-03-07 22:18:40 +0000

    simplifications to 06 fitting script

--- R/06_Multivariate_Models/06_Fitting_a_multivariate_normal_and_t.R
+++ R/06_Multivariate_Models/06_Fitting_a_multivariate_normal_and_t.R
@@ -2,7 +2,7 @@
 
 ## Fitting a multivariate normal and t distribution to Dow Jones -log-returns
 
-library(quantmod) # for index()
+library(xts) # for time series manipulation
 library(qrmdata) # for Dow Jones constituents data
 library(QRM) # for fit.mst()
 library(mvtnorm) # for sampling from a multivariate normal or t distribution
@@ -10,13 +10,14 @@ library(mvtnorm) # for sampling from a multivariate normal or t distribution
 ## Load and extract the data we work with
 data(DJ_const)
 str(DJ_const)
-S <- DJ_const[index(DJ_const) >= "2000-01-01", c("AAPL", "BA", "INTC", "IBM", "NKE")]
+S <- DJ_const['2000-01-01/', c("AAPL", "BA", "INTC", "IBM", "NKE")]
 ## => We work with Apple, Boeing, Intel, IBM, Nike since 2000-01-01 here
+plot.zoo(S)
 
 ## Build and plot -log-returns
-X. <- -apply(log(S), 2, diff) # compute -log-returns and range
-X <- as.xts(X.)
-pairs(X., main="Scatter plot matrix of risk-factor changes", gap=0, pch=".")
+X <- -diff(log(S))[-1,] # compute -log-returns 
+
+pairs(as.matrix(X), main="Scatter plot matrix of risk-factor changes", gap=0, pch=".")
 plot.zoo(X, xlab="Time", main="")
 ## For illustration purposes, we treat this data as realizations of iid
 ## five-dimensional random vectors.
@@ -39,4 +40,4 @@ cols <- rep(adjustcolor(c("black", "royalblue3", "maroon3"), alpha.f=0.5), each=
 pairs(dat, gap=0, pch=".", col=cols)
 ## It's hard to see but the multivariate t fits better. To see this, consider
 ## just the first pair.
-plot(dat[,1:2], col=cols) # => the multivariate normal generates to few extreme losses!
+plot(dat[,1:2], col=cols) # => the multivariate normal generates too few extreme losses!
