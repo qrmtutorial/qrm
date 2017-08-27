414acc162f78fe4aa20d4b74ab0909f781a02d59
Marius Hofert <marius.hofert@uwaterloo.ca>
2017-08-26 22:13:42 -0400

    updated script

--- R/09_Market_Risk/09_Standard_methods_for_market_risk.R
+++ R/09_Market_Risk/09_Standard_methods_for_market_risk.R
@@ -44,7 +44,7 @@ plot(X, cex = 0.4)
 
 ##' @title Compute the Loss Operator
 ##' @param x Matrix of risk-factor changes
-##' @param weights Weights w = lambda_j * S_{t,j}
+##' @param weights Weights \tilde{w} = lambda_j * S_{t,j}
 ##' @return Losses
 ##' @author Marius Hofert
 loss_operator <- function(x, weights)
@@ -60,7 +60,7 @@ loss_operator <- function(x, weights)
 ##'         possibly other results (depending on the estimator)
 ##' @author Marius Hofert
 risk_measure <- function(S, lambda, alpha,
-                         method = c("Var.Cov", "hist.sim", "MC.N", "MC.t", "GPD"),
+                         method = c("Var.Cov", "hist.sim", "MC.N", "MC.t", "POT"),
                          ...)
 {
     ## Input checks and conversions
@@ -73,7 +73,7 @@ risk_measure <- function(S, lambda, alpha,
     X <- returns(S) # compute risk-factor changes
     if(!length(X)) stop("'S' should have more than just one line") # check
     S. <- as.numeric(tail(S, n = 1)) # pick out last available stock prices ("today")
-    w <- lambda * S. # weights w today
+    w. <- lambda * S. # weights \tilde{w} today
 
     ## Method switch (now consider the various methods)
     switch(method,
@@ -81,8 +81,8 @@ risk_measure <- function(S, lambda, alpha,
                ## Estimate a multivariate normal distribution
                mu.hat <- colMeans(X) # estimate the mean vector mu
                Sigma.hat  <- var(X) # estimate the covariance matrix Sigma
-               L.delta.mean <- -sum(w * mu.hat) # mean of the approx. normal df of the linearized loss L^{\Delta}
-               L.delta.sd <- sqrt(t(w) %*% Sigma.hat %*% w) # corresponding standard deviation
+               L.delta.mean <- -sum(w. * mu.hat) # mean of the approx. normal df of the linearized loss L^{\Delta}
+               L.delta.sd <- sqrt(t(w.) %*% Sigma.hat %*% w.) # corresponding standard deviation
                ## Compute VaR and ES and return
                qa <- qnorm(alpha)
                list(VaR = L.delta.mean + L.delta.sd * qa,
@@ -94,7 +94,7 @@ risk_measure <- function(S, lambda, alpha,
            },
            "hist.sim" = { # historical simulation method
                ## Using nonparametrically estimated risk measures
-               L <- loss_operator(X, weights = w) # compute historical losses
+               L <- loss_operator(X, weights = w.) # compute historical losses
                ## Nonparametrically estimate VaR and ES and return
                list(VaR = VaR_np(L, alpha),
                     ES  =  ES_np(L, alpha))
@@ -105,7 +105,7 @@ risk_measure <- function(S, lambda, alpha,
                mu.hat <- colMeans(X) # estimate the mean vector mu
                Sigma.hat  <- var(X) # estimate the covariance matrix Sigma
                X. <- rmvnorm(N, mean = mu.hat, sigma = Sigma.hat) # simulate risk-factor changes
-               L <- loss_operator(X., weights = w) # compute corresponding (simulated) losses
+               L <- loss_operator(X., weights = w.) # compute corresponding (simulated) losses
                ## Compute VaR and ES and return
                list(VaR = VaR_np(L, alpha), # nonparametrically estimate VaR
                     ES  =  ES_np(L, alpha), # nonparametrically estimate ES
@@ -118,7 +118,7 @@ risk_measure <- function(S, lambda, alpha,
                N <- list(...)$N # pick out N from '...'
                fit <- fit.mst(X, method = "BFGS") # fit a multivariate t distribution
                X. <- rmvt(N, sigma = as.matrix(fit$Sigma), df = fit$df, delta = fit$mu) # simulate risk-factor changes
-               L <- loss_operator(X., weights = w) # compute corresponding (simulated) losses
+               L <- loss_operator(X., weights = w.) # compute corresponding (simulated) losses
                ## Compute VaR and ES and return
                list(VaR = VaR_np(L, alpha), # nonparametrically estimate VaR
                     ES  =  ES_np(L, alpha), # nonparametrically estimate ES
@@ -128,9 +128,9 @@ risk_measure <- function(S, lambda, alpha,
                     Sigma = fit$covariance, # fitted covariance matrix
                     df    = fit$df) # fitted degrees of freedom
            },
-           "GPD" = { # simulate losses from a fitted Generalized Pareto distribution (GPD); this is underlying the peaks-over-threshold method
+           "POT" = { # simulate losses from a fitted Generalized Pareto distribution (GPD); this is underlying the peaks-over-threshold (POT) method
                stopifnot(hasArg(q)) # check if the quantile-threshold 'q' has been provided
-               L. <- loss_operator(X, weights = w) # historical losses
+               L. <- loss_operator(X, weights = w.) # historical losses
                u <- quantile(L., probs = list(...)$q, names = FALSE) # determine the threshold as the q-quantile of the historical losses
                fit <- fit.GPD(L., threshold = u) # fit a GPD to the excesses (note: fit.GPD() requires the losses)
                xi <- fit$par.ests[["xi"]] # fitted xi
@@ -168,23 +168,23 @@ set.seed(271) # set a seed so that all simulation results are reproducible; see 
 var.cov  <- risk_measure(S, lambda = lambda, alpha = alpha, method = "Var.Cov")
 hist.sim <- risk_measure(S, lambda = lambda, alpha = alpha, method = "hist.sim")
 MC.N     <- risk_measure(S, lambda = lambda, alpha = alpha, method = "MC.N", N = N)
-GPD      <- risk_measure(S, lambda = lambda, alpha = alpha, method = "GPD", N = N, q = 0.9)
+POT      <- risk_measure(S, lambda = lambda, alpha = alpha, method = "POT", N = N, q = 0.9)
 MC.t     <- risk_measure(S, lambda = lambda, alpha = alpha, method = "MC.t", N = N)
 
 ## Pick out VaR and ES for all methods
 (rm <- rbind("MC (normal)"    = unlist(MC.N[c("VaR", "ES")]),
              "Var.-cov."      = unlist(var.cov),
              "Hist. sim."     = unlist(hist.sim),
-             "GPD"            = unlist(GPD [c("VaR", "ES")]),
+             "POT"            = unlist(POT [c("VaR", "ES")]),
              "MC (Student t)" = unlist(MC.t[c("VaR", "ES")])))
 
-### Graphical goodness-of-fit check for the GPD
+### Graphical goodness-of-fit check for the fitted GPD
 
 ## Transform the excesses with the fitted GPD distribution function.
 ## The resulting sample should roughly follow a standard uniform distribution.
-excess <- GPD$excess # excesses over the threshold
-xi.hat <- GPD$xi # estimated xi
-beta.hat <- GPD$beta # estimated beta
+excess <- POT$excess # excesses over the threshold
+xi.hat <- POT$xi # estimated xi
+beta.hat <- POT$beta # estimated beta
 z <- pGPD(excess, xi = xi.hat, beta = beta.hat) # should be U[0,1]
 plot(z, ylab = "Fitted GPD applied to the excesses") # looks fine
 
@@ -198,8 +198,8 @@ qq_plot(excess, FUN = function(p) qGPD(p, xi = xi.hat, beta = beta.hat),
 
 ## Compute historical losses (for creating a histogram); see risk.measure()
 S. <- as.numeric(tail(S, n = 1)) # pick out last available stock prices ("today")
-w <- lambda * S. # weights w
-L <- loss_operator(X, weights = w) # historical losses
+w. <- lambda * S. # weights \tilde{w}
+L <- loss_operator(X, weights = w.) # historical losses
 summary(L) # get important statistics about the losses
 
 ## Plot a histogram of the losses including the VaR and ES estimates
@@ -225,10 +225,10 @@ legend("topright", bty = "n", inset = 0.02, lty = lty, lwd = lwd,
 ##   method.
 ## - The historical simulation method, however, implies that the loss
 ##   distribution is more heavy-tailed. This is captured quite well by
-##   GPD-based method and (possibly too well by) the Monte Carlo method for
+##   POT method and (possibly too well by) the Monte Carlo method for
 ##   multivariate t distributed risk-factor changes
 ##   (degrees of freedom are MC.t$df ~= 2.4).
 ## - It's overall reassuring that several methods (historical simulation,
-##   GPD-based EVT approach and -- with slight departure for ES --
+##   POT method and -- with slight departure for ES --
 ##   MC for a Student t) lead to similar results. Somewhere in this range,
 ##   one can then determine an adequate amount of risk capital.
