99e70d6a1d0ee02de00131caf542e46ba7033497
Marius Hofert <mhofert@users.noreply.github.com>
2019-12-03 23:37:14 -0500

    adapted a script

--- code/11_Portfolio_Credit_Risk_Management/11_Credit_estimation.R
+++ code/11_Portfolio_Credit_Risk_Management/11_Credit_estimation.R
@@ -1,64 +1,73 @@
-## By Alexander McNeil
+## By Alexander McNeil and Marius Hofert
 
-library(QRM)
-library(lme4)
+## Estimating parameters of portfolio models from default data
 
-data(spdata.raw)
-spdata.raw
-attach(spdata.raw)
 
-# Calculate default rates
+### Setup ######################################################################
 
-BdefaultRate <- Bdefaults/Bobligors
-BBdefaultRate <- BBdefaults/BBobligors
-BBBdefaultRate <- BBBdefaults/BBBobligors
-AdefaultRate <- Adefaults/Aobligors
-CCCdefaultRate <- CCCdefaults/CCCobligors
+library(QRM) # for momest(), fit.binomial(), fit.binomialProbitnorm()
+library(qrmdata)
+library(lme4) # for glmer()
 
-# Plot default rates
 
-year <- 1981:2000
-plot(year,CCCdefaultRate,xlab="Year",ylab="Rate",type="l")
-lines(year,BdefaultRate,col=2)
-lines(year,BBdefaultRate,col=3)
-lines(year,BBBdefaultRate,col=4)
-lines(year,AdefaultRate,col=5)
+### Data preparation ###########################################################
 
-# simple moment estimator
-pY <- momest(Bdefaults,Bobligors)
-rhoY <- (pY[2]-pY[1]^2)/(pY[1]-pY[1]^2)
+## Load
+data(SP_defaults)
+str(SP_defaults)
 
-# Fit binomial model followed by one factor model
-mod0 <- fit.binomial(Bdefaults, Bobligors)
-mod1 <- fit.binomialProbitnorm(Bdefaults, Bobligors)
+## Pick out defaults, firms, and ratings
+defaults <- SP_defaults[,"Defaults",]
+firms <- SP_defaults[,"Obligors",]
+rating <- colnames(firms)
+
+## Calculate default rates
+(defRate <- apply(SP_defaults, c(1, 3), function(x) x["Defaults"]/x["Obligors"]))
+nrate <- ncol(defRate)
+
+## Plot default rates
+years <- as.numeric(format(as.Date(dimnames(SP_defaults)$Time), "%Y"))
+pal <- colorRampPalette(c("black", "royalblue3", "darkorange2", "maroon3"), space = "Lab")
+cols <- pal(nrate)
+plot(NA, xlim = range(years), ylim = range(defRate),
+     xlab = "Year", ylab = "Default rate")
+for(r in 1:nrate)
+    lines(years, defRate[,r], type = "l", col = cols[r])
+legend("topleft", bty = "n", lty = rep(1, nrate), col = cols, legend = rating)
+
+
+### Fitting ####################################################################
+
+## Simple moment estimator (for 'B')
+pY <- momest(defaults[,"B"], firms[,"B"])
+rhoY <- (pY[2]-pY[1]^2) / (pY[1]-pY[1]^2)
+
+## Binomial model
+mod0 <- fit.binomial(defaults[,"B"], firms[,"B"])
+
+## One-factor model
+mod1 <- fit.binomialProbitnorm(defaults[,"B"], firms[,"B"])
+
+## Compare the latter two models
 c(mod0$maxloglik, mod1$maxloglik)
 
-# Advanced
-# Gives table 8.8 in first edition of book
 
-# Change format of data for glmm
+## More advanced (gives MFE (2005, Table 8.8))
 
-defaultmatrix <- cbind(Adefaults,BBBdefaults,BBdefaults,Bdefaults,CCCdefaults)
-firmmatrix <- cbind(Aobligors,BBBobligors,BBobligors,Bobligors,CCCobligors)
+## Prepare data for glmer()
+df <- data.frame(defaults = as.vector(t(defaults)), firms = as.vector(t(firms)),
+                 year = rep(years, each = 5),
+                 rating = factor(rep(rating, 20), levels = rating, ordered = TRUE))
+tail(df, n = 10)
 
-defaults <- as.vector(t(defaultmatrix))
-firms <- as.vector(t(firmmatrix))
-year <- rep(year,rep(5,20))
-rating <- factor(rep(c("A","BBB","BB","B","CCC"),20),levels=c("A","BBB","BB","B","CCC"),ordered=TRUE)
+## Fit a glmm
+(mod <- glmer(cbind(df$defaults, df$firms - df$defaults) ~ -1 + df$rating + (1 | df$year),
+              family = binomial(probit)))
 
-tail(data.frame(defaults,firms,year,rating),n=10)
-
-# Fit glmm
-
-(mod <- glmer(cbind(defaults,firms-defaults) ~ -1 + rating + (1|year),family=binomial(probit)))
-
-# Compute implied PDs and asset correlation
-
+## Compute implied PDs and asset correlation
 sigma <- mod@theta
-mu <- mod@beta
-
 beta <- sigma^2/(1+sigma^2)
-# Asset correlation. Much smaller than the numbers coming out of equity analysis
 
-PD <- pnorm(mu*sqrt(1-beta))
-
+## Asset correlation. Much smaller than the numbers coming out of equity analysis
+mu <- mod@beta
+(PD <- pnorm(mu * sqrt(1 - beta)))
